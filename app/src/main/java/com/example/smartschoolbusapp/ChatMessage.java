878ee62ae0f1876e7d959114a58f469faf481b6c
Vasanth Sai Aitha <62834887+VasanthsaiAitha@users.noreply.github.com>
2025-03-22 20:26:15 -0500

    updated Chat Message dialog_create_route.xml

--- app/src/main/java/com/example/smartschoolbusapp/ChatMessage.java
+++ app/src/main/java/com/example/smartschoolbusapp/ChatMessage.java
@@ -1,19 +1,26 @@
 package com.example.smartschoolbusapp;
 
-public class ChatMessage {
+public class ChatMessage extends ChatItem {
     private String senderID;
     private String receiverID;
     private String message;
     private long timestamp;
 
     public ChatMessage() { }
-
+    @Override
+    public int getType() {
+        return TYPE_MESSAGE;
+    }
     public ChatMessage(String senderID, String receiverID, String message, long timestamp) {
         this.senderID = senderID;
         this.receiverID = receiverID;
         this.message = message;
         this.timestamp = timestamp;
     }
+    private boolean seen;
+
+    public boolean isSeen() { return seen; }
+    public void setSeen(boolean seen) { this.seen = seen; }
 
     public String getSenderID() { return senderID; }
     public String getReceiverID() { return receiverID; }
